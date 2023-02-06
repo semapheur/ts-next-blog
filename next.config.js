957e5c3fb13ab23e60a2f6545744bbeb43c2af4f
semapheur <danfyll@hotmail.com>
2023-02-06 01:56:35 +0100

    Deploy

--- next.config.js
+++ next.config.js
@@ -5,24 +5,22 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   //assetPrefix: isProd ? '/ts-next-blog' : '',
   //basePath: '/ts-next-blog',
+  reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
-  //webpack: (config, options) => {
-  //  if (options.isServer) {
-  //    return {
-  //      ...config,
-  //      entry() {
-  //        return config.entry().then(entry => {
-  //          return Object.assign({}, entry, {
-  //            cache: './scripts/cache.ts'
-  //          })
-  //        })
-  //      }
-  //    }
-  //  }
-  //  return config;
-  //},
+  webpack: (config, options) => {
+    if (!options.dev && !options.isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      })
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
