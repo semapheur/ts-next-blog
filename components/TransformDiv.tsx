662ec5469d5e33e767eb16df7d94899a8636c2d0
semapheur <danfyll@hotmail.com>
2023-11-23 20:30:47 +0100

    Deploy

--- components/TransformDiv.tsx
+++ components/TransformDiv.tsx
@@ -1,13 +1,20 @@
 'use client'
 
 import { MouseEvent, useState, useRef, HTMLProps, ReactNode } from 'react'
+import {Signal} from '@preact/signals-react'
 import {Vec2} from 'utils/types'
 
+type Transform2D = {
+  scale: Vec2
+  translate: Vec2
+}
+
 interface Props extends HTMLProps<HTMLDivElement> {
+  transform: Signal<Transform2D>
   children: ReactNode
 }
 
-export default function TransformDiv({children, ...props}: Props) {
+export default function TransformDiv({transform, children, ...props}: Props) {
   const divRef = useRef<HTMLDivElement>(null)
   const [isDragging, setDragging] = useState(false)
   const [startPos, setStartPos] = useState<Vec2>({x: 0, y: 0})
@@ -23,10 +30,8 @@ export default function TransformDiv({children, ...props}: Props) {
   const handleMouseMove = (e: MouseEvent) => {
     if (isDragging) {
 
-      const delta = {
-        x: e.clientX - startPos.x,
-        y: e.clientY - startPos.y
-      }
+      transform.value.translate.x += e.clientX - startPos.x
+      transform.value.translate.y += e.clientY - startPos.y
 
       setStartPos({
         x: e.clientX,
