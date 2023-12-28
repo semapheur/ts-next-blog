3d50f95ada56d1990431b844e905ec082f0d577e
semapheur <danfyll@hotmail.com>
2023-12-28 14:00:36 +0100

    Deploy

--- components/PeriodicTable.tsx
+++ components/PeriodicTable.tsx
@@ -10,16 +10,29 @@ export default async function PeriodicTable() {
     grid-cols-[repeat(18,minmax(0,1fr))] 
     grid-rows-[repeat(10,minmax(0,1fr))]'
   >
-    {data.map(element => <div key={element.name} style={{
-      gridColumn: element.xpos,
-      gridRow: element.ypos
+    {data.map(element => <div key={element.name} 
+      className='relative grid' 
+      style={{
+        gridColumn: element.xpos,
+        gridRow: element.ypos,
+        backgroundColor: `rgb(var(--color-${element.group_block?.replaceAll(' ', '-')}))`
     }}>
-        {element.symbol}
+        <span className='absolute top-0 left-1 text-text text-sm'>
+          {element.number}
+        </span>
+        <span className='m-auto text-text text-2xl'>
+          {element.symbol}
+        </span>
+        <span className='absolute bottom-1 left-1/2 -translate-x-1/2 text-text text-xs'>
+          {element.name}
+        </span>
+        
       </div>)}
   </div>)
 }
 
 /*
+
 <div key={element.name} className='h-full' style={{
         gridColumn: element.xpos,
         gridRow: element.ypos
