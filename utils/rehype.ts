999d6b45a976e50133888757f50bb5816696a267
semapheur <danfyll@hotmail.com>
2024-06-25 20:38:05 +0200

    Deploy

--- utils/rehype.ts
+++ utils/rehype.ts
@@ -1,66 +1,73 @@
-import { Element, Root } from 'hast'
-import {hasProperty} from 'hast-util-has-property'
-import {Transformer} from 'unified'
-import {selectAll, select} from 'hast-util-select'
-import {visit} from 'unist-util-visit'
+import type { Root } from 'hast'
+import type { Transformer } from 'unified'
+import { selectAll, select } from 'hast-util-select'
+import { visit } from 'unist-util-visit'
 
 export function rehypeMathref(): Transformer<Root, Root> {
   const boxCount = {
-    'algorithm': 0,
-    'axiom': 0,
-    'conjecture': 0,
-    'corollary': 0,
-    'criteria': 0,
-    'definition': 0,
-    'example': 0,
-    'lemma': 0,
-    'observation': 0,
-    'property': 0,
-    'proposition': 0,
-    'proof': 0,
-    'remark': 0,
-    'theorem': 0
+    algorithm: 1,
+    axiom: 1,
+    conjecture: 1,
+    corollary: 1,
+    criteria: 1,
+    definition: 1,
+    example: 1,
+    lemma: 1,
+    observation: 1,
+    property: 1,
+    proposition: 1,
+    proof: 1,
+    remark: 1,
+    theorem: 1,
   }
 
   return (root) => {
-
     const eqTags = selectAll('span[id].enclosing', root)
 
-    for (let i=0; i < eqTags.length; i++) {
+    for (let i = 0; i < eqTags.length; i++) {
       const id = eqTags[i].properties.id
 
-      const eqLabel = select(`span[id=${id}].enclosing > span.mord.text > span.mord`, eqTags[i])
-      eqLabel!.children = [{type: 'text', value: `${i}`}]
+      const eqLabel = select(
+        `span[id=${id}].enclosing > span.mord.text > span.mord`,
+        eqTags[i],
+      )
+      eqLabel!.children = [{ type: 'text', value: `${i + 1}` }]
 
-      const eqRefs = selectAll(`a[href="#${id}"] > span.mord.text > span.mord`, root)
-      
-      for (let j=0; j < eqRefs.length; j++) {
-        eqRefs[j].children = [{type: 'text', value: `${i}`}]
+      const eqRefs = selectAll(
+        `a[href="#${id}"] > span.mord.text > span.mord`,
+        root,
+      )
+
+      for (let j = 0; j < eqRefs.length; j++) {
+        eqRefs[j].children = [{ type: 'text', value: `${i + 1}` }]
       }
     }
 
     visit(root, 'mdxJsxFlowElement', (node) => {
       if (node.name !== 'MathBox') return
-      
+
       let id = ''
       let boxType = ''
       for (const attribute of node.attributes) {
         if (!('name' in attribute)) continue
 
         if (attribute.name === 'boxType') {
-          boxType = (attribute.value as string)
+          boxType = attribute.value as string
         }
         if (attribute.name === 'tag') {
-          id = (attribute.value as string)
+          id = attribute.value as string
         }
       }
       if (!id || !(boxType in boxCount)) {
         if (boxType in boxCount) boxCount[boxType]++
         return
       }
-      const boxRefs = selectAll(`a[href="#${id}"] > span.mord.text > span.mord`, root)
-      for (let i=0; i < boxRefs.length; i++) {
-        boxRefs[i].children = [{type: 'text', value: `${boxCount[boxType]}`}]
+      const boxRefs = selectAll(
+        `a[href="#${id}"] > span.mord.text > span.mord`,
+        root,
+      )
+      for (let i = 0; i < boxRefs.length; i++) {
+        boxRefs[i].children = [{ type: 'text', value: `${boxCount[boxType]}` }]
       }
       boxCount[boxType]++
     })
