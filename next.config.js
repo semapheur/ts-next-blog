f6199155e573e233478dcfc04e448d2e2492a123
semapheur <danfyll@hotmail.com>
2025-07-07 21:07:14 +0200

    Deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const million = require('million/compiler')
+const { default: next } = require('next')
 
 const isProd = process.env.NODE_ENV === 'production'
 
