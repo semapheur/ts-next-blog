9dd455c48a21f3f4d2f97b2727b9d8256cd4698e
semapheur <danfyll@hotmail.com>
2025-10-24 00:11:45 +0200

    Backup

--- next.config.js
+++ next.config.js
@@ -48,12 +48,29 @@ const nextConfig = {
       },
     ]
   },
-  webpack: (config, _) => {
-    config.module.rules.push({
-      test: /\.(glsl|fs|vs|frag|vert)$/,
-      use: ['raw-loader']
-    })
-    return config
+  turbopack: {
+    rules: {
+      '*.glsl': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+      '*.fs': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+      '*.vs': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+      '*.frag': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+      '*.vert': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+    },
   },
 }
 
