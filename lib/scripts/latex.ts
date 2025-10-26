db7eae9e87558e0a029086a6e1f25596d6858a00
semapheur <danfyll@hotmail.com>
2025-10-26 22:38:28 +0100

    Backup

--- lib/scripts/latex.ts
+++ lib/scripts/latex.ts
@@ -191,5 +191,5 @@ function relabelLatexEquations(mdxFile: string) {
   })
 }
 
-console.log(findLatexIssues("./content/notes/physics/quantum_mechanics.mdx"))
+console.log(findLatexIssues("./content/notes/physics/quantum_formalism.mdx"))
 //relabelLatexEquations("./content/notes/math/differential_geometry.mdx")
