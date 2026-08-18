4d6025d7575ee8b310c4686e2b36d0e02984cc41
semapheur <danfyll@hotmail.com>
2026-08-18 23:28:38 +0200

    Backup

--- lib/scripts/latex.ts
+++ lib/scripts/latex.ts
@@ -394,7 +394,7 @@ function relabelLatexEquations(mdxFile: string) {
 }
 console.log(
   findLatexIssues(
-    "./content/notes/physics/quantum_mechanics/many-body_systems/hartree-fock_method.mdx",
+    "./content/notes/physics/quantum_mechanics/many-body_systems/perturbation_theory.mdx",
   ),
 );
 
